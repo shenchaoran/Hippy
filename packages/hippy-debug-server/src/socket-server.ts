1411fbeec46756ff07fbb6311f9f95dc5d071283
chestershen <chestershen@tencent.com>
2021-08-13 20:16:59 +0800

    fix: 消息上行至devtools串了
    - 多devtool ws client 时（有插件时），应保证下行id全局唯一，上行时根据id上行到请求方；如果是事件，则全部上行

--- packages/hippy-debug-server/src/socket-server.ts
+++ packages/hippy-debug-server/src/socket-server.ts
@@ -16,12 +16,14 @@ const debug = createDebug('socket-bridge');
 createDebug.enable('socket-bridge');
 
 export class SocketServer extends DomainRegister {
-  private appWsMap = new Map<string, WebSocket>();
-  private appClientListMap: Map<string, AppClient[]> = new Map();
+  // key: appClientId
+  private connectionMap: Map<string, Connection> = new Map();
   private wsPath: string;
   private wss: Server;
   private server;
   private debugTarget: DebugTarget;
+  // key: req id
+  private idWsMap: Map<number, WebSocket> = new Map();
 
   constructor(server, { wsPath }) {
     super();
@@ -59,8 +61,15 @@ export class SocketServer extends DomainRegister {
     if (!debugTarget) return;
     this.debugTarget = debugTarget;
     const appClientId = debugTarget.id;
-    let appClientList = this.appClientListMap.get(appClientId);
-    if (!appClientList?.length) {
+    if (!this.connectionMap.has(appClientId)) {
+      this.connectionMap.set(appClientId, {
+        appClientList: [],
+        devtoolsWsList: [],
+        appWs: undefined,
+      });
+    }
+    const conn = this.connectionMap.get(appClientId);
+    if (!conn.appClientList?.length) {
       let options;
       if (debugTarget.platform === DevicePlatform.Android) {
         options = appClientManager.getAndroidAppClientOptions();
@@ -68,7 +77,7 @@ export class SocketServer extends DomainRegister {
         options = appClientManager.getIosAppClientOptions();
       }
 
-      appClientList = options
+      conn.appClientList = options
         .map(({ Ctor, ...option }: AppClientFullOptionOmicCtx) => {
           const urlParsedContext = debugTarget2UrlParsedContext(debugTarget);
           const newOption: AppClientOption = {
@@ -76,7 +85,7 @@ export class SocketServer extends DomainRegister {
             ...option,
           };
           if (Ctor.name === AppClientType.WS) {
-            newOption.ws = this.appWsMap.get(appClientId);
+            newOption.ws = conn.appWs;
             if (!newOption.ws) {
               debug('no app ws connection, ignore WsAppClient.');
               return;
@@ -85,21 +94,26 @@ export class SocketServer extends DomainRegister {
           return new Ctor(appClientId, newOption);
         })
         .filter((v) => v);
-      this.appClientListMap.set(appClientId, appClientList);
     }
+    if (ws) conn.devtoolsWsList.push(ws);
 
     // 绑定数据上行
-    appClientList.forEach((appClient) => {
+    conn.appClientList.forEach((appClient) => {
       appClient.removeAllListeners(ClientEvent.Message);
       appClient.on(ClientEvent.Message, (msg: Adapter.CDP.Res) => {
         // 上行到监听器
         this.triggerListerner(msg);
         // 上行到devtools
-        if (ws) {
-          if ('id' in msg && msg.id < 0) {
-            return;
-          }
-          ws.send(JSON.stringify(msg));
+        if ('id' in msg) {
+          // command 上行到调用的 ws
+          const currentWs = this.idWsMap.get(msg.id);
+          currentWs.send(JSON.stringify(msg));
+          this.idWsMap.delete(msg.id);
+        } else {
+          // event 无法判断是谁触发的，上行到每一个 ws。客户端如果不关心该事件，就不会注册监听，不会有影响。
+          conn.devtoolsWsList.forEach((ws) => {
+            ws.send(JSON.stringify(msg));
+          });
         }
       });
     });
@@ -107,20 +121,22 @@ export class SocketServer extends DomainRegister {
     // 绑定数据下行
     if (ws) {
       ws.on('message', (msg: string) => {
+        // appClient 和 ws 是一对多的关系（插件会单独建立一个通道），所以appClient收到消息后需查询上行到哪个ws
         const msgObj: Adapter.CDP.Req = JSON.parse(msg);
-        appClientList.forEach((appClient) => {
+        this.idWsMap.set(msgObj.id, ws);
+        conn.appClientList.forEach((appClient) => {
           appClient.send(msgObj);
         });
       });
 
       ws.on('close', () => {
-        appClientList.forEach((appClient) => {
+        conn.appClientList.forEach((appClient) => {
           appClient.resumeApp();
         });
       });
     }
 
-    return appClientList;
+    return conn.appClientList;
   }
 
   private onConnection(ws, req) {
@@ -130,9 +146,18 @@ export class SocketServer extends DomainRegister {
     debug('debug page: %j', debugTarget);
     debug('%s connected!', clientRole);
 
-    if (pathname !== this.wsPath) return debug('invalid ws connection path!');
-    if (clientRole === ClientRole.Devtools && !debugTarget) return debug("debugTarget doesn't exist!");
-    if (!Object.values(ClientRole).includes(clientRole)) return debug('invalid client role!');
+    if (pathname !== this.wsPath) {
+      ws.close();
+      return debug('invalid ws connection path!');
+    }
+    if (clientRole === ClientRole.Devtools && !debugTarget) {
+      ws.close();
+      return debug("debugTarget doesn't exist!");
+    }
+    if (!Object.values(ClientRole).includes(clientRole)) {
+      ws.close();
+      return debug('invalid client role!');
+    }
 
     if (clientRole === ClientRole.Devtools) {
       this.selectDebugTarget(debugTarget, ws);
@@ -141,7 +166,19 @@ export class SocketServer extends DomainRegister {
       if (platform === DevicePlatform.Android) {
         androidDebugTargetManager.addWsTarget(clientId);
       }
-      this.appWsMap.set(clientId, ws);
+      if (!this.connectionMap.has(clientId)) {
+        this.connectionMap.set(clientId, {
+          appClientList: [],
+          devtoolsWsList: [],
+          appWs: ws,
+        });
+      }
     }
   }
 }
+
+type Connection = {
+  appClientList: AppClient[];
+  devtoolsWsList: WebSocket[];
+  appWs?: WebSocket;
+};
